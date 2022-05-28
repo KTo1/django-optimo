9e2ab79ccbbd9e587e1322ea57c219e59d030db0
kto <kto@lovetou.ru>
2022-05-28 17:44:56 +0700

    бага

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -3,10 +3,11 @@ from basketapp.models import Basket
 
 def basket(request):
     baskets = []
+    basket_total_sum = 0
+    basket_total_quantity = 0
+
     if request.user.is_authenticated:
         baskets = Basket.objects.filter(user=request.user).select_related()
-        basket_total_sum = 0
-        basket_total_quantity = 0
         for basket in baskets:
             basket_total_sum += basket.quantity * basket.product.price
             basket_total_quantity += basket.quantity
