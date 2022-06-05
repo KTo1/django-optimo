6e2a33bc82ac70e50f669b07406d8f83a91b34ef
KTo <kto@lovetou.ru>
2022-06-05 14:36:51 +0700

    тесты методов моделей

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -3,7 +3,7 @@ from mainapp.models import Products, ProductCategories
 from django.test.client import Client
 
 
-class TestMainSmokeT(TestCase):
+class TestMainSmoke(TestCase):
 
     def setUp(self) -> None:
         category = ProductCategories.objects.create(name='Test')
@@ -21,3 +21,33 @@ class TestMainSmokeT(TestCase):
 
     def tearDown(self) -> None:
         pass
+
+
+class ProductsTestCase(TestCase):
+
+    def setUp(self):
+        category = ProductCategories.objects.create(name="стулья")
+        self.product_1 = Products.objects.create(name="стул 1", category=category, price=1999.5, quantity=150)
+        self.product_2 = Products.objects.create(name="стул 2", category=category, price=2998.1, quantity=125, is_active=False)
+        self.product_3 = Products.objects.create(name="стул 3", category=category, price=998.1, quantity=115)
+
+    def test_product_get(self):
+
+        product_1 = Products.objects.get(name="стул 1")
+        product_2 = Products.objects.get(name="стул 2")
+        self.assertEqual(product_1, self.product_1)
+        self.assertEqual(product_2, self.product_2)
+
+    def test_product_print(self):
+
+        product_1 = Products.objects.get(name="стул 1")
+        product_2 = Products.objects.get(name="стул 2")
+        self.assertEqual(str(product_1), 'стул 1')
+        self.assertEqual(str(product_2), 'стул 2')
+
+    # def test_product_get_items(self):
+    #
+    #     product_1 = Products.objects.get(name="стул 1")
+    #     product_3 = Products.objects.get(name="стул 3")
+    #     products = product_1.get_items()
+    #     self.assertEqual(list(products), [product_1, product_3])
